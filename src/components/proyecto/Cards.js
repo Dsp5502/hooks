dc742e31bf80ce64c274eb2c426c17751e2c5dcc
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-22 18:38:43 -0500

    img ghypji4

--- src/components/proyecto/Cards.js
+++ src/components/proyecto/Cards.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card } from 'react-bootstrap';
+// import { Button, Card } from 'react-bootstrap';
 
 const Cards = ({ nombre }) => {
   const [imagenes, setImagenes] = useState([]);
